fd856f7233d851ace08ef66dfce5128ee446e16f
Oliver Etchebarne <yo@drmad.org>
2022-05-31 18:18:29 -0500

    Add CSS and JS assets processors routes

--- routes/main.php
+++ routes/main.php
@@ -8,4 +8,8 @@ return [
 
     // Change this rule for your default view or controller
     Rule::default()->view('welcome'),
+
+    // CSS and JS assets processors
+    Rule::path('assets')->includeFromClass(AssetManager\Controller::class),
+
 ];
