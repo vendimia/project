d8c4dbb1eac8a0537d1ae62d5a761c33b9729644
Oliver Etchebarne <yo@drmad.org>
2022-12-01 14:44:48 -0500

    debug is now setted on development config

---
+++ config/default.development.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * This file contains configuration parameters that will override the main
+ * configuration when Vendimia\ENVIRONMENT const has 'development' value.
+ *
+ * This is the default value if no environment variable VENDIMIA_ENVIRONMENT
+ * is present.
+ */
+return [
+    'debug' => true,
+];
--- config/default.php
+++ config/default.php
@@ -1,6 +1,4 @@
 <?php return [
-    'debug' => true,
-
     'session' => [
         'driver' => Vendimia\Session\Driver\Php::class,
         'options' => [
